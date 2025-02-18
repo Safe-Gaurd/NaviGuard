1f3eff64533f517fc24bfcdad99eb3947ca223d9
LokeshSuryaPrakashK <lokeshsuryaprakashk@gmail.com>
2025-02-18 19:32:52 +0530

    Image Picker Updation

--- lib/constants/image_picker.dart
+++ lib/constants/image_picker.dart
@@ -2,16 +2,6 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 
-pickImage(ImageSource source) async {
-  final ImagePicker imagePicker = ImagePicker();
-  XFile? file = await imagePicker.pickImage(source: source);
-  if (file != null) {
-    return await file.readAsBytes();
-  } else {
-    //print(" no image is selectd");
-  }
-}
-
 class CustomImagePicker {
   final bool isReport;
   File? imageFile;
